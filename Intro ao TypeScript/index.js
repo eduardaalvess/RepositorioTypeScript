85e04513eab250f14a68ca12b744dbf54fd203e3
saintravi <Dudah4@gmail.com>
2021-08-14 06:49:28 -0300

    first commit

---
+++ Intro ao TypeScript/index.js
@@ -0,0 +1,8 @@
+"use strict";
+function somar(num1, num2) {
+    return num1 + num2;
+}
+var numero1 = 10;
+var numero2 = 5;
+var resultado = somar(numero1, numero2);
+console.log(resultado);
---
+++ Intro ao TypeScript/index.ts
@@ -0,0 +1,10 @@
+function somar (num1: number, num2: number) {
+    return num1 + num2;
+}
+
+const numero1 = 10;
+const numero2 = 5;
+
+const resultado = somar(numero1, numero2);
+
+console.log(resultado);
